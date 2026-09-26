f53e52c8b9b95f6ceba7c0fe9fb430648707730a
redcloudyun-cmyk <redcloudyun@gmail.com>
2026-09-26 23:40:18 +0900

    feat(r23.4v): add inject-only credential broker contract

---
+++ src/security/credentials/index.ts
@@ -0,0 +1,9 @@
+export { CredentialBrokerService } from './credential-broker.service.js';
+export type {
+  CredentialInjectionContext,
+  CredentialLease,
+  CredentialReference,
+  CredentialSecretResolver,
+  CredentialStatus,
+  CredentialUseRequest,
+} from './credential.types.js';
